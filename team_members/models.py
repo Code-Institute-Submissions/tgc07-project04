cd4d8f1a572f4c9431d2b9ccda11a4a5328706a1
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-09 09:03:23 +0800

    Create Team and TeamMember models

--- team_members/models.py
+++ team_members/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Team(models.Model):
+    team_name = models.CharField(max_length=50, blank=False)
+
+    def __str__(self):
+        return self.team_name
+
+class TeamMember(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_admin = models.BooleanField(blank=False, default=False)
+    is_project_manager = models.BooleanField(blank=False, default=False)
+    team = models.ForeignKey(Team, on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return self.user.username
