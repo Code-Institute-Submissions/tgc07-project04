8a2cb03b46e6b32b38f7fe5fdf4b5f615c9d5005
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-09 13:08:04 +0800

    Create Task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from team_members.models import TeamMember
+
 # Create your models here.
 class Stage(models.Model):
     label = models.CharField(max_length=20, blank=False)
@@ -18,4 +20,19 @@ class SeverityLevel(models.Model):
 
     def __str__(self):
         return self.severity_level
+
+class Task(models.Model):
+    title = models.CharField(max_length=50, blank=False)
+    description = models.TextField(blank=True, null=True)
+    date_due = models.DateField(blank=True, null=True)
+    task_creator = models.ForeignKey(TeamMember, on_delete=models.RESTRICT)
+    stage = models.ForeignKey(Stage, on_delete=models.RESTRICT)
+    priority_level = models.ForeignKey(
+        PriorityLevel, on_delete=models.RESTRICT)
+    severity_level = models.ForeignKey(
+        SeverityLevel, on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return self.title
+
 
