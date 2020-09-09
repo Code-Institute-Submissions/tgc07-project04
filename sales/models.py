a03e1c07e5eb59d2804c2ae503d0835cb8fb7206
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-09 14:20:31 +0800

    Create Service and Transaction models

--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+from team_members.models import Team
+
 # Create your models here.
+class Service(models.Model):
+    service_name = models.CharField(max_length=50, blank=False)
+    price = models.IntegerField(blank=False)
+    service_description = models.TextField(blank=False)
+
+    def __str__(self):
+        return self.service_name
+
+class Transaction(models.Model):
+    date = models.DateField(blank=False)
+    team = models.ForeignKey(Team, on_delete=models.RESTRICT)
+    service = models.ForeignKey(Service, on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return f'{self.date} - {self.team.team_name} - {self.service.service_name}'
