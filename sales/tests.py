ec456d21e703235e0eeb072601b9e368e9096560
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-09 14:11:08 +0800

    Define test to check Service model values

--- sales/tests.py
+++ sales/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.shortcuts import get_object_or_404
+
+from .models import *
+from team_members.models import Team
 
 # Create your tests here.
+class ServiceModelTestCase(TestCase):
+    def test_values(self):
+        test_s = Service(
+            service_name = "1 User",
+            price = 12345,
+            service_description = "Teams consisting of only a single user"
+        )
+        test_s.save()
+
+        db_test_s = get_object_or_404(Service, pk=test_s.id)
+
+        self.assertEqual(db_test_s.service_name, test_s.service_name)
+        self.assertEqual(db_test_s.price, test_s.price)
+        self.assertEqual(
+            db_test_s.service_description, test_s.service_description)
+
