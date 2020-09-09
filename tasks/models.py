d52eabb8cbb9b98c4b3a38a8a17cd8e313409832
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-09 11:28:43 +0800

    Create Stage, PriorityLevel, SeverityLevel models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Stage(models.Model):
+    label = models.CharField(max_length=20, blank=False)
+
+    def __str__(self):
+        return self.label
+
+class PriorityLevel(models.Model):
+    priority_level = models.CharField(max_length=20, blank=False)
+
+    def __str__(self):
+        return self.priority_level
+
+class SeverityLevel(models.Model):
+    severity_level = models.CharField(max_length=20, blank=False)
+
+    def __str__(self):
+        return self.severity_level
+
