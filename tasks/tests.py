8fd863fcad261ac6d420a2e7afc3fc583a50adbf
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-09 11:18:00 +0800

    Define test to check Stage Model values

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.shortcuts import get_object_or_404
+
+from .models import *
 
 # Create your tests here.
+class StageModelTestCase(TestCase):
+    def test_values(self):
+        test_stage = Stage(label="Completed Stage")
+        test_stage.save()
+
+        db_test_stage = get_object_or_404(Stage, pk=test_stage.id)
+
+        self.assertEqual(db_test_stage.label, test_stage.label)
+
