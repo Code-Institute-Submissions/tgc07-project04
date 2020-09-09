7574c5c019f7e8dfb51e89835ee6c55c8807e075
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-09 08:58:32 +0800

    Define test to check TeamMember model default values

--- team_members/tests.py
+++ team_members/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+
+from .models import *
 
 # Create your tests here.
+class TeamMemberModelTestCase(TestCase):
+    def setUp(self):
+        self.team = Team(team_name="TestTeam")
+        self.team.save()
+    
+    def test_default_values(self):
+        test_user = User(
+            username = "username_test",
+            email = "email_test@mailinator.com",
+            password = "pass123word",
+        )
+        test_user.save()
+
+        test_team_member = TeamMember(
+            user = test_user,
+            team = self.team
+            )
+        test_team_member.save()
+        
+        db_test_user = get_object_or_404(TeamMember, pk=test_user.id)
+
+        print(__name__)
+        self.assertEqual(db_test_user.is_admin, False)
+        self.assertEqual(db_test_user.is_project_manager, False)
+
