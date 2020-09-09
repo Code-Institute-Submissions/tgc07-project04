b6201fbffd9ee853c181d938d9ea8e28881be50c
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-09 14:42:15 +0800

    Register models in admin portal

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Stage)
+admin.site.register(PriorityLevel)
+admin.site.register(SeverityLevel)
+admin.site.register(Task)
+admin.site.register(ChecklistItem)
+admin.site.register(TaskAssignee)
