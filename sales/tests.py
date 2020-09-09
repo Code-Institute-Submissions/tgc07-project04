ab6c70f8811ece1a008ddc50bc588133ea8122f7
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-09 14:19:09 +0800

    Define test to check Transaction model values

--- sales/tests.py
+++ sales/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.shortcuts import get_object_or_404
 
+from datetime import date
+
 from .models import *
 from team_members.models import Team
 
@@ -21,3 +23,28 @@ class ServiceModelTestCase(TestCase):
         self.assertEqual(
             db_test_s.service_description, test_s.service_description)
 
+class TransactionModelTestCase(TestCase):
+    def setUp(self):
+        self.team = Team(team_name="TestTeam")
+        self.team.save()
+
+        self.service = Service(
+            service_name = "1 User",
+            price = 12345,
+            service_description = "Teams consisting of only a single user"
+        )
+        self.service.save()
+    
+    def test_values(self):
+        test_t = Transaction(
+            date = date.today(),
+            team = self.team,
+            service = self.service
+        )
+        test_t.save()
+
+        db_test_t = get_object_or_404(Transaction, pk=test_t.id)
+
+        self.assertEqual(db_test_t.date, test_t.date)
+        self.assertEqual(db_test_t.team, test_t.team)
+        self.assertEqual(db_test_t.service, test_t.service)
