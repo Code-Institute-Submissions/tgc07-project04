03fd29af446879fa4256039fdc894e0df17c892e
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-10 13:42:07 +0800

    Update Team model

--- team_members/models.py
+++ team_members/models.py
@@ -4,15 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Team(models.Model):
     team_name = models.CharField(max_length=50, blank=False)
+    team_member = models.ManyToManyField(User, related_name="team_member")
+    admin_user = models.ManyToManyField(User, related_name="admin_user")
+    project_manager = models.ManyToManyField(User, related_name="project_manager")
 
     def __str__(self):
         return self.team_name
-
-class TeamMember(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    is_admin = models.BooleanField(blank=False, default=False)
-    is_project_manager = models.BooleanField(blank=False, default=False)
-    team = models.ForeignKey(Team, on_delete=models.RESTRICT)
-
-    def __str__(self):
-        return self.user.username
