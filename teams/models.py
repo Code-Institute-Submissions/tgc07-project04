82f1b79c8516ce0c52999a9bbf4671053401c02f
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-10 17:42:28 +0800

    Update teams models to use extra fields on many-to-many relationships

--- teams/models.py
+++ teams/models.py
@@ -4,9 +4,15 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Team(models.Model):
     team_name = models.CharField(max_length=50, blank=False)
-    team_member = models.ManyToManyField(User, related_name="team_member")
-    admin_user = models.ManyToManyField(User, related_name="admin_user")
-    project_manager = models.ManyToManyField(User, related_name="project_manager")
+    team_member = models.ManyToManyField(User, through="Membership")
 
     def __str__(self):
         return self.team_name
+
+class Membership(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    is_admin = models.BooleanField(blank=True, default=False)
+
+    def __str__(self):
+        return f"User:{self.user}, Team:{self.team}, is_admin:{self.is_admin}"
