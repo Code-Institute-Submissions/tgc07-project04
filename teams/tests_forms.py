f974d3267983c554bc06aa2f456a15ecf114275c
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-10 17:41:18 +0800

    Update form tests for change in Team model

--- teams/tests_forms.py
+++ teams/tests_forms.py
@@ -12,35 +12,17 @@ class TeamFormTestCase(TestCase):
         )
         self.team_member.save()
 
-        self.admin_user = User(
-            username = "test_admin_user",
-            email = "admin_user@mailinator.com",
-            password = "pass123word"
-        )
-        self.admin_user.save()
-    
-        self.project_manager = User(
-            username = "test_project_manager",
-            email = "project_manager@mailinator.com",
-            password = "pass123word"
-        )
-        self.project_manager.save()
-    
     def test_form_valid(self):
         form = TeamForm({
             "team_name": "My Test Team",
-            "team_member" : [self.team_member],
-            "admin_user" : [self.admin_user],
-            "project_manager" : [self.project_manager]
+            "team_member" : [self.team_member]
         })
         self.assertTrue(form.is_valid())
 
     def test_team_name_required(self):
         form = TeamForm({
             "team_name": None,
-            "team_member" : [self.team_member],
-            "admin_user" : [self.admin_user],
-            "project_manager" : [self.project_manager]
+            "team_member" : [self.team_member]
         })
         self.assertFalse(form.is_valid())
         self.assertIn('team_name', form.errors.keys())
@@ -50,39 +32,13 @@ class TeamFormTestCase(TestCase):
     def test_team_member_required(self):
         form = TeamForm({
             "team_name": "My Test Team",
-            "team_member" : [],
-            "admin_user" : [self.admin_user],
-            "project_manager" : [self.project_manager]
+            "team_member" : None
         })
         self.assertFalse(form.is_valid())
         self.assertIn('team_member', form.errors.keys())
         self.assertEqual(
             form.errors['team_member'][0], 'This field is required.')
 
-    def test_admin_user_required(self):
-        form = TeamForm({
-            "team_name": "My Test Team",
-            "team_member" : [self.team_member],
-            "admin_user" : [],
-            "project_manager" : [self.project_manager]
-        })
-        self.assertFalse(form.is_valid())
-        self.assertIn('admin_user', form.errors.keys())
-        self.assertEqual(
-            form.errors['admin_user'][0], 'This field is required.')
-
-    def test_project_manager_required(self):
-        form = TeamForm({
-            "team_name": "My Test Team",
-            "team_member" : [self.team_member],
-            "admin_user" : [self.admin_user],
-            "project_manager" : []
-        })
-        self.assertFalse(form.is_valid())
-        self.assertIn('project_manager', form.errors.keys())
-        self.assertEqual(
-            form.errors['project_manager'][0], 'This field is required.')
-    
     def test_form_meta_fields(self):
         form = TeamForm()
         self.assertEqual(form.Meta.fields, '__all__')
