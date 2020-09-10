19203901f18333a4680e5f0a6b579e0d1262c1c5
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-10 18:35:07 +0800

    Define tests to check TransactionForm validity

--- sales/tests_forms.py
+++ sales/tests_forms.py
@@ -4,6 +4,7 @@ from datetime import date
 
 from .models import *
 from .forms import *
+from teams.models import Team
 
 class ServiceFormTestCase(TestCase):
     def test_form_valid(self):
@@ -27,4 +28,43 @@ class ServiceFormTestCase(TestCase):
         self.assertEqual(
             form.errors['service_description'][0], 'This field is required.')
 
+class TransactionFormTestCase(TestCase):
+    def setUp(self):
+        self.team_member = User(
+            username = "test_team_member",
+            email = "team_member@mailinator.com",
+            password = "pass123word"
+        )
+        self.team_member.save()
 
+        self.team = Team(team_name = "Test Team")
+        self.team.save()
+        self.team.team_member.add(self.team_member)
+    
+        self.service = Service(
+            service_name = "1 User",
+            price = 12345,
+            service_description = "Teams consisting of only a single user"
+        )
+        self.service.save()
+
+    def test_form_valid(self):
+        form = TransactionForm({
+            "date": date.today(),
+            "team" : self.team,
+            "service" : self.service
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_required_field(self):
+        form = TransactionForm({})
+        self.assertFalse(form.is_valid())
+        self.assertIn('date', form.errors.keys())
+        self.assertEqual(
+            form.errors['date'][0], 'This field is required.')
+        self.assertIn('team', form.errors.keys())
+        self.assertEqual(
+            form.errors['team'][0], 'This field is required.')
+        self.assertIn('service', form.errors.keys())
+        self.assertEqual(
+            form.errors['service'][0], 'This field is required.')
