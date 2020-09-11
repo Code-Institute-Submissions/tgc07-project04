d1ba1674ff9e765de5dfeefedc3e681e31e70513
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-11 15:44:58 +0800

    Add unique constraints to teams models

--- teams/models.py
+++ teams/models.py
@@ -6,6 +6,12 @@ class Team(models.Model):
     team_name = models.CharField(max_length=50, blank=False)
     team_members = models.ManyToManyField(User, through="Membership")
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['team_name'], name='unique_team_name')
+        ]
+
     def __str__(self):
         return self.team_name
 
@@ -13,6 +19,12 @@ class Membership(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     is_admin = models.BooleanField(blank=True, default=False)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'team'], name='unique_membership')
+        ]
 
     def __str__(self):
         return f"User:{self.user}, Team:{self.team}, is_admin:{self.is_admin}"
