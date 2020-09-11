a815ea93b8e1a3eecda854ef6aa9e6eb17ac6957
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-11 21:01:40 +0800

    Define test to check create team view

--- teams/tests_views.py
+++ teams/tests_views.py
@@ -1,13 +1,50 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import User
 
-from .models import Team
+from .models import *
 
 class CreateTeamViewTestCase(TestCase):
+    def setUp(self):
+        self.team_member = User(
+            username = "test_team_member",
+            email = "team_member@mailinator.com",
+            password = "pass123word"
+        )
+        self.team_member.save()
+
+        self.team = Team(team_name="Test Team Name")
+        self.team.save()
+
+        self.membership_model = Membership(
+                user = self.team_member,
+                team = self.team,
+                is_admin = True
+        )
+        self.membership_model.save()
+    
     def test_get_response(self):
         response = self.client.get(reverse('create_team_route'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'teams/create-team.html')
+
+    def test_create_team(self):
+        response = self.client.post(reverse('create_team_route'), {
+            "team_name" : self.team,
+            "user" : str(self.team_member.id),
+            "team" : str(self.team.id),
+            "is_admin" : True
+        })
+        self.assertEqual(response.status_code, 200)
+        db_team = Team.objects.filter(team_name="Test Team Name")
+        self.assertEqual(db_team.count(), 1)
+        db_membership_user = Membership.objects.filter(user=str(
+            self.team_member.id))
+        self.assertEqual(db_membership_user.count(), 1)
+        db_membership_team = Membership.objects.filter(team=str(self.team.id))
+        self.assertEqual(db_membership_team.count(), 1)
+        db_membership_is_admin = Membership.objects.filter(is_admin=True)
+        self.assertEqual(db_membership_is_admin.count(), 1)
 
 class UpdateTeamViewTestCase(TestCase):
     def setUp(self):
