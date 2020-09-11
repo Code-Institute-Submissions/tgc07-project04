c9c3c115485222dfd11ff402f6828dacaf7953c9
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-11 17:19:23 +0800

    Define test to check update team view

--- teams/tests_views.py
+++ teams/tests_views.py
@@ -1,8 +1,21 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from .models import Team
+
 class CreateTeamViewTestCase(TestCase):
     def test_get_response(self):
         response = self.client.get(reverse('create_team_route'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'teams/create-team.html')
+
+class UpdateTeamViewTestCase(TestCase):
+    def setUp(self):
+        self.team = Team(team_name="Test Team Name")
+        self.team.save()
+    
+    def test_get_response(self):
+        response = self.client.get(reverse(
+            'update_team_route', kwargs={'team_id':self.team.id}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'teams/update-team.html')
