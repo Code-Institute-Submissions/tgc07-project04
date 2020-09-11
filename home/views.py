c0c3daad334cb669c1d45db77e38d2fa7130bf78
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-11 13:53:19 +0800

    Add test home page displaying current logged in user info

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    if request.user.is_authenticated:
+        print(request.user.id)
+        print(request.user.username)
+        print(request.user.email)
+    return render(request, 'home/index.html')
