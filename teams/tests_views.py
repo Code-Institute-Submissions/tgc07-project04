a1645eedb8e9ebbe3649464fadedde3c7e21faa6
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-12 23:48:06 +0800

    Define tests to check update membership view

--- teams/tests_views.py
+++ teams/tests_views.py
@@ -146,7 +146,7 @@ class CreateMembershipViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'teams/create-membership.html')
 
-    def test_create_team(self):
+    def test_create_membership(self):
         new_team_member = User(
             username = "new_team_member",
             email = "new_team_member@mailinator.com",
@@ -156,9 +156,9 @@ class CreateMembershipViewTestCase(TestCase):
         
         response = self.client.post(reverse('create_membership_route',
             kwargs={'team_id':self.team.id}), {
-            "user" : str(new_team_member.id),
-            "team" : str(self.team.id),
-            "is_admin" : True
+                "user" : str(new_team_member.id),
+                "team" : str(self.team.id),
+                "is_admin" : True
         })
         # Check redirect
         self.assertEqual(response.status_code, 302)
@@ -176,4 +176,70 @@ class CreateMembershipViewTestCase(TestCase):
         # Check that there are 2 memberships matching is_admin query
         db_membership_is_admin = Membership.objects.filter(is_admin=True)
         self.assertEqual(db_membership_is_admin.count(), 2)
+
+class UpdateMembershipViewTestCase(TestCase):
+    def setUp(self):
+        # Create user instance
+        self.team_member = User(
+            username = "test_team_member",
+            email = "team_member@mailinator.com",
+            password = "pass123word"
+        )
+        self.team_member.save()
+        # Log in user
+        self.client.force_login(self.team_member, backend=None)
+
+        self.new_team_member = User(
+            username = "new_team_member",
+            email = "new_team_member@mailinator.com",
+            password = "pass123word"
+        )
+        self.new_team_member.save()
+
+        # Create team instance
+        self.team = Team(team_name="Test Team Name")
+        self.team.save()
+
+        # Create membership instance
+        self.membership_model = Membership(
+                user = self.team_member,
+                team = self.team,
+                is_admin = True
+        )
+        self.membership_model.save()
+    
+    def test_get_response(self):
+        response = self.client.get(reverse('update_membership_route', kwargs={
+            'team_id': self.team.id,
+            'membership_id': self.membership_model.id
+            }))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'teams/update-membership.html')
+
+    def test_update_membership(self):
+        response = self.client.post(reverse('update_membership_route',
+            kwargs={
+                'team_id': self.team.id,
+                'membership_id': self.membership_model.id
+            }), {
+                'user': str(self.new_team_member.id),
+                'team': str(self.team.id),
+                'is_admin': True
+        })
+        # Check redirect
+        self.assertEqual(response.status_code, 302)
+        # Check that there is only 1 membership in team queried
+        db_membership_team = Membership.objects.filter(team=self.team.id)
+        self.assertEqual(db_membership_team.count(), 1)
+        # Check that there is no memberships matching queried user
+        db_membership_user_1 = Membership.objects.filter(user=str(
+            self.team_member.id))
+        self.assertEqual(db_membership_user_1.count(), 0)
+        # Check that there is only 1 membership matching queried user
+        db_membership_user_2 = Membership.objects.filter(user=str(
+            self.new_team_member.id))
+        self.assertEqual(db_membership_user_2.count(), 1)
+        # Check that there are no memberships matching is_admin query
+        db_membership_is_admin = Membership.objects.filter(is_admin=True)
+        self.assertEqual(db_membership_is_admin.count(), 1)
     
