4d5df72d01d69830b17b105a368b77d0ab02a740
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-12 17:02:57 +0800

    Update MembershipForm testing as team field will not be needed in the form

--- teams/tests_forms.py
+++ teams/tests_forms.py
@@ -31,51 +31,37 @@ class TeamFormTestCase(TestCase):
         form = TeamForm()
         self.assertEqual(form.Meta.fields, ['team_name'])
 
-# class MembershipFormTestCase(TestCase):
-#     def setUp(self):
-#         self.team_member = User(
-#             username = "test_team_member",
-#             email = "team_member@mailinator.com",
-#             password = "pass123word"
-#         )
-#         self.team_member.save()
+class MembershipFormTestCase(TestCase):
+    def setUp(self):
+        self.team_member = User(
+            username = "test_team_member",
+            email = "team_member@mailinator.com",
+            password = "pass123word"
+        )
+        self.team_member.save()
 
-#         self.team = Team(team_name="Test Team Name")
-#         self.team.save()
+        self.team = Team(team_name="Test Team Name")
+        self.team.save()
 
-#     def test_form_valid(self):
-#         form = MembershipForm({
-#             "team_members": [self.team_member],
-#             "team_name" : self.team,
-#             "is_admin" : True
-#         })
-#         self.assertTrue(form.is_valid())
+    def test_form_valid(self):
+        form = MembershipForm({
+            "user" : self.team_member,
+            "is_admin" : True
+        })
+        self.assertTrue(form.is_valid())
 
-    # def test_team_member_required(self):
-    #     form = MembershipForm({
-    #         "team_members": None,
-    #         "team_name" : self.team,
-    #         "is_admin" : True
-    #     })
-    #     self.assertFalse(form.is_valid())
-    #     self.assertIn('team_members', form.errors.keys())
-    #     self.assertEqual(
-    #         form.errors['team_members'][0], 'This field is required.')
+    def test_team_member_required(self):
+        form = MembershipForm({
+            "user": None,
+            "is_admin" : True
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('user', form.errors.keys())
+        self.assertEqual(
+            form.errors['user'][0], 'This field is required.')
 
-    # def test_team_name_required(self):
-    #     form = MembershipForm({
-    #         "team_members": [self.team_member],
-    #         "team_name" : None,
-    #         "is_admin" : True
-    #     })
-    #     self.assertFalse(form.is_valid())
-    #     self.assertIn('team_name', form.errors.keys())
-    #     self.assertEqual(
-    #         form.errors['team_name'][0], 'This field is required.')
-
-    # def test_admin_boolean_not_required(self):
-    #     form = MembershipForm({
-    #         "team_members": [self.team_member],
-    #         "team_name" : self.team
-    #     })
-    #     self.assertTrue(form.is_valid())
+    def test_admin_boolean_not_required(self):
+        form = MembershipForm({
+            "user": self.team_member
+        })
+        self.assertTrue(form.is_valid())
