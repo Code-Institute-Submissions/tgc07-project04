1298bb4af91e6b83189e9e158e50402f8c48c986
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-12 20:51:20 +0800

    Rename routes for consistency

--- teams/urls.py
+++ teams/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 import teams.views
 
 urlpatterns = [
-    path('create-team', teams.views.create_team, name='create_team_route'),
-    path('update-team/<team_id>', teams.views.update_team,
+    path('create-team/', teams.views.create_team, name='create_team_route'),
+    path('<team_id>/update-team/', teams.views.update_team,
         name="update_team_route"),
-    path('delete-team/<team_id>', teams.views.delete_team,
+    path('<team_id>/delete-team/', teams.views.delete_team,
         name="delete_team_route"),
     path('<team_id>/create-membership/', teams.views.create_membership,
         name='create_membership_route'),
