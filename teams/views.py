25b0f9c210163c8ed9d292efaa86be30a73860f9
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-12 18:55:53 +0800

    Handle IntegrityError when creating memberships

--- teams/views.py
+++ teams/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.db.utils import IntegrityError
 
 from .forms import *
 from .models import *
@@ -19,9 +20,8 @@ def create_team(request):
                 is_admin = True
             )
             membership_model.save()
-            messages.success(
-                request, f"Team {submitted_form.cleaned_data['team_name']} \
-                    has been created")
+            messages.add_message(request, messages.SUCCESS, f"Team \
+                {submitted_form.cleaned_data['team_name']} has been created")
             return redirect(reverse('home_route'))
         else:
             return render(request, "teams/create-team.html", {
@@ -40,9 +40,8 @@ def update_team(request, team_id):
         form = TeamForm(request.POST, instance=team_to_update)
         if form.is_valid():
             form.save()
-            messages.success(
-                request, f"Updated team name to \
-                    {form.cleaned_data['team_name']}")
+            messages.add_message(request, messages.SUCCESS, f"Updated team \
+                name to {form.cleaned_data['team_name']}")
             return redirect(reverse('home_route'))
         else:
             return render(request, 'teams/update-team.html', {
@@ -75,11 +74,20 @@ def create_membership(request, team_id):
         if submitted_form.is_valid():
             membership_model = submitted_form.save(commit=False)
             membership_model.team = get_object_or_404(Team, pk=team_id)
-            membership_model.save()
-            messages.success(
-                request, f"Membership with {membership_model.team} \
-                    has been created")
-            return redirect(reverse('home_route'))
+            try:
+                # Try to create membership
+                membership_model.save()
+                messages.add_message(request, messages.SUCCESS, f"Membership \
+                    with {membership_model.team} has been created")
+                return redirect(reverse('home_route'))
+            except IntegrityError:
+                # If error (e.g. unique constraint failed) then flash message
+                messages.add_message(request, messages.WARNING, f"Membership \
+                    with {membership_model.team} already exists. Please \
+                        select another user.")
+                return render(request, "teams/create-membership.html", {
+                'form': submitted_form
+            })
         else:
             return render(request, "teams/create-membership.html", {
                 'form': submitted_form
@@ -96,6 +104,6 @@ def create_membership(request, team_id):
             })
         # If no matches, or current user is not admin
         else:
-            messages.success(
-                request, "Sorry, you do not have the necessary access rights")
+            messages.add_message(request, messages.WARNING, "Sorry, you do \
+                not have the necessary access rights to view that page")
             return redirect(reverse('account_login'))
