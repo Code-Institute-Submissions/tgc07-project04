aa28a5c005d888e68c9a66cd4f667bfb41f68574
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-14 13:05:37 +0800

    Add basic create task view

--- tasks/views.py
+++ tasks/views.py
@@ -12,13 +12,20 @@ from teams.models import *
 # @login_required
 def create_task(request, team_id):
     if request.method == "POST":
-        submitted_form = Task(request.POST)
+        submitted_form = TaskForm(request.POST)
 
         if submitted_form.is_valid():
-            model = submitted_form.save()
-            messages.add_message(request, messages.SUCCESS, f"Team \
-                {submitted_form.cleaned_data['team_name']} has been created")
-            return redirect(reverse('home_route'))
+            model = submitted_form.save(commit=False)
+            model.team = get_object_or_404(Team, pk=team_id)
+            model.task_creator = request.user
+            model.save()
+            # Save first before adding ManyToMany entries because need id
+            for assignee in submitted_form.cleaned_data.get('assignee').all():
+                model.assignee.add(assignee)
+            messages.add_message(request, messages.SUCCESS, f"Task \
+                {submitted_form.cleaned_data['title']} has been created")
+            return redirect(reverse(
+                'create_task_route', kwargs={'team_id':team_id}))
         else:
             return render(request, "tasks/create-task.html", {
                 'form': submitted_form
