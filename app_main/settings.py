1120c333dd8f2486a6f29a453997dbc467964058
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-14 17:34:07 +0800

    Add static files directory

--- app_main/settings.py
+++ app_main/settings.py
@@ -135,6 +135,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 AUTHENTICATION_BACKENDS = (
     # Needed to login by username in Django admin, regardless of `allauth`
