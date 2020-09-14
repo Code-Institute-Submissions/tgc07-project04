b67a26ff2a8942717e0f2a29826dd0bb976ff2b2
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-14 14:04:27 +0800

    Show different get responses to create task get requests depending on whether current user is member of team and is_admin

--- tasks/views.py
+++ tasks/views.py
@@ -12,29 +12,50 @@ from teams.models import *
 # @login_required
 def create_task(request, team_id):
     if request.method == "POST":
-        submitted_form = TaskForm(request.POST)
+        form = TaskForm(request.POST)
 
-        if submitted_form.is_valid():
-            model = submitted_form.save(commit=False)
+        if form.is_valid():
+            model = form.save(commit=False)
             model.team = get_object_or_404(Team, pk=team_id)
             model.task_creator = request.user
             model.save()
             # Save first before adding ManyToMany entries because need id
-            for assignee in submitted_form.cleaned_data.get('assignee').all():
+            for assignee in form.cleaned_data.get('assignee').all():
                 model.assignee.add(assignee)
             messages.add_message(request, messages.SUCCESS, f"Task \
-                {submitted_form.cleaned_data['title']} has been created")
+                {form.cleaned_data['title']} has been created")
             return redirect(reverse(
                 'create_task_route', kwargs={'team_id':team_id}))
         else:
             return render(request, "tasks/create-task.html", {
-                'form': submitted_form
+                'form': form
             })
+
+    # request.method == "GET"
     else:
         form = TaskForm()
-        # Filter assignee list by members of team
-        form.fields['assignee'].queryset = User.objects.filter(
-            user_membership__team=team_id)
-        return render(request, 'tasks/create-task.html', {
-            'form': form
-        })
+        # Query database membership matches for team_id and current user
+        db_membership = Membership.objects.filter(team=team_id).filter(
+            user=request.user)
+        # If match found and current user is_admin
+        if db_membership and db_membership[0].is_admin:
+            # Filter assignee list by members of team
+            form.fields['assignee'].queryset = User.objects.filter(
+                user_membership__team=team_id)
+            return render(request, 'tasks/create-task.html', {
+                'form': form
+            })
+        # If current user is a member, but NOT admin
+        elif db_membership and not db_membership[0].is_admin:
+            # Only show self in list of assignees
+            form.fields['assignee'].queryset = User.objects.filter(
+                id=request.user.id)
+            return render(request, 'tasks/create-task.html', {
+                'form': form
+            })
+        # If not member, redirect
+        else:
+            messages.add_message(request, messages.WARNING, "Sorry, you do \
+                not have the necessary access rights to view that page")
+            return redirect(reverse('account_login'))
+
