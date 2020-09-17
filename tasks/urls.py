1614fc8d030441686df4d9501f0bb25a6531aa77
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-17 12:00:05 +0800

    Add basic view and route for single task

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 import tasks.views
 
 urlpatterns = [
-    path('<team_id>/team/', tasks.views.tasks_team,
-        name='tasks_team_route'),
+    path('<team_id>/team/', tasks.views.tasks_team, name='tasks_team_route'),
+    path('<team_id>/team/<task_id>/', tasks.views.view_single_task,
+        name='task_single_route'),
     path('<team_id>/create-task/', tasks.views.create_task,
         name='create_task_route'),
     path('<team_id>/update-task/<task_id>/', tasks.views.update_task,
