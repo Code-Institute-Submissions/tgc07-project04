7026a891bd221dc8fff469fbd3bdc957052787d5
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-17 14:44:21 +0800

    Define test to check read user membership view

--- teams/tests_views.py
+++ teams/tests_views.py
@@ -350,4 +350,35 @@ class DeleteMembershipViewTestCase(TestCase):
         # Check matching is_admin query
         db_membership_is_admin = Membership.objects.filter(is_admin=True)
         self.assertEqual(db_membership_is_admin.count(), 1)
+
+class ReadUserMembershipViewTestCase(TestCase):
+    def setUp(self):
+        # Create user instance
+        self.team_member = User(
+            username = "test_team_member",
+            email = "team_member@mailinator.com",
+            password = "pass123word"
+        )
+        self.team_member.save()
+        # Log in user
+        self.client.force_login(self.team_member, backend=None)
+
+        # Create team instance
+        self.team = Team(team_name="Test Team Name")
+        self.team.save()
+
+        # Create membership instance
+        self.membership_model = Membership(
+                user = self.team_member,
+                team = self.team,
+                is_admin = True
+        )
+        self.membership_model.save()
     
+    def test_get_response(self):
+        response = self.client.get(reverse('user_memberships_route'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'teams/read-user-memberships.html')
+
+
+
