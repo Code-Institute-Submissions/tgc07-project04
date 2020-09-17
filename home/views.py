f216daf0dce2b589a60a73e829f8a98455856ae8
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-17 18:15:29 +0800

    Display demo kanban tasks summary on home page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 
+from tasks.models import *
+
 def index(request):
     if request.user.is_authenticated:
         print(request.user.id)
         print(request.user.username)
         print(request.user.email)
-    return render(request, 'home/index.html')
+
+    tasks = {}
+    tasks_team = Task.objects.filter(team=1)
+    stages = Stage.objects.all()
+    for stage in stages:
+        tasks.update({
+            stage.id: {
+                'stage_label': stage.label,
+                'tasks': tasks_team.filter(
+                    stage=stage.id)
+            }
+        })
+    return render(request, 'home/index.html', {
+        'tasks': tasks,
+        'team_id': None,
+        'demo': True
+    })
+
