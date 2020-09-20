f969ddb2d63b118a1cd1e64df768d32d465e49a5
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-20 14:27:17 +0800

    Add transactional stripe and user data fields to Transaction model

--- sales/models.py
+++ sales/models.py
@@ -1,8 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 from teams.models import Team
 
-# Create your models here.
 class Service(models.Model):
     service_name = models.CharField(max_length=50, blank=False)
     price = models.IntegerField(blank=False)
@@ -15,6 +15,10 @@ class Transaction(models.Model):
     date = models.DateField(blank=False)
     team = models.ForeignKey(Team, on_delete=models.RESTRICT)
     service = models.ForeignKey(Service, on_delete=models.RESTRICT)
+    transaction_ref = models.CharField(blank=False, max_length=100)
+    stripe_webhook_id = models.CharField(blank=False, max_length=100)
+    paid_by_user = models.ForeignKey(User, on_delete=models.RESTRICT)
 
     def __str__(self):
-        return f'{self.date} - {self.team.team_name} - {self.service.service_name}'
+        return f'{self.date} - {self.team.team_name} - \
+            {self.service.service_name}'
