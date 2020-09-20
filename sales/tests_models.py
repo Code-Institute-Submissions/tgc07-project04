05ab95afb786919bfc7869044695527af8cc4f56
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-20 21:58:14 +0800

    Update sales models testing for change in sales model

--- sales/tests_models.py
+++ sales/tests_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 
 from datetime import date
 
@@ -10,9 +11,10 @@ from teams.models import Team
 class ServiceModelTestCase(TestCase):
     def test_values(self):
         test_s = Service(
-            service_name = "1 User",
-            price = 12345,
-            service_description = "Teams consisting of only a single user"
+            service_name = "30 day subscription",
+            price = 1000,
+            service_description = "Price per team per 30 days. Add as many \
+                team members as you like!"
         )
         test_s.save()
 
@@ -29,17 +31,30 @@ class TransactionModelTestCase(TestCase):
         self.team.save()
 
         self.service = Service(
-            service_name = "1 User",
-            price = 12345,
-            service_description = "Teams consisting of only a single user"
+            service_name = "1 year subscription",
+            price = 10000,
+            service_description = "Price per team per year. Pay annually and \
+                save over 20%! Unlimited number of team members."
         )
         self.service.save()
     
+        self.payer = User(
+            username = "test_task_creator",
+            email = "team_member@mailinator.com",
+            password = "pass123word"
+        )
+        self.payer.save()
+        # Log in user
+        self.client.force_login(self.payer, backend=None)
+
     def test_values(self):
         test_t = Transaction(
             date = date.today(),
             team = self.team,
-            service = self.service
+            service = self.service,
+            transaction_ref = "qhfo837hdnq23hdqi3hdq3hdq",
+            stripe_webhook_id = "hdqi73hdhwdhqw3hdfqkiw3",
+            paid_by_user = self.payer
         )
         test_t.save()
 
@@ -48,3 +63,6 @@ class TransactionModelTestCase(TestCase):
         self.assertEqual(db_test_t.date, test_t.date)
         self.assertEqual(db_test_t.team, test_t.team)
         self.assertEqual(db_test_t.service, test_t.service)
+        self.assertEqual(db_test_t.transaction_ref, test_t.transaction_ref)
+        self.assertEqual(db_test_t.stripe_webhook_id, test_t.stripe_webhook_id)
+        self.assertEqual(db_test_t.paid_by_user, test_t.paid_by_user)
