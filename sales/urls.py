8abe2722de4a35ef2a72dfa02bf59a6d90e39848
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-20 14:57:28 +0800

    Add basic select subscription template and link url route

--- sales/urls.py
+++ sales/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 import sales.views
 
 urlpatterns = [
+    path('subscription/<team_id>', sales.views.select_subscription,
+        name='checkout_select_subscription_route'),
 ]
