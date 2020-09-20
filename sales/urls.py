778ceb1c23bb1a7c2f84f40def35021191395c4e
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-20 15:37:43 +0800

    Add stripe checkout for purchases and save transaction data to database

--- sales/urls.py
+++ sales/urls.py
@@ -5,4 +5,12 @@ import sales.views
 urlpatterns = [
     path('subscription/<team_id>', sales.views.select_subscription,
         name='checkout_select_subscription_route'),
+    path('stripe/', sales.views.checkout_stripe,
+        name='checkout_stripe_route'),
+    path('success/', sales.views.checkout_success,
+        name='checkout_success_route'),
+    path('cancelled/', sales.views.checkout_cancelled,
+        name='checkout_cancelled_route'),
+    path('payment-completed/', sales.views.payment_completed,
+        name='payment_completed_route'),
 ]
