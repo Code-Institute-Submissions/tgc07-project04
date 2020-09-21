0339627364f51c3236724402d4ea09e844ca1840
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-21 16:46:21 +0800

    Remove test output on index.html template and console print

--- home/views.py
+++ home/views.py
@@ -3,11 +3,6 @@ from django.shortcuts import render
 from tasks.models import *
 
 def index(request):
-    if request.user.is_authenticated:
-        print(request.user.id)
-        print(request.user.username)
-        print(request.user.email)
-
     tasks = {}
     tasks_team = Task.objects.filter(team=1)
     stages = Stage.objects.all()
