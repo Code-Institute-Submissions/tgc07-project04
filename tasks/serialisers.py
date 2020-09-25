58669b8ac36617c7dc9cd9f00a0c1fac7af1d9f6
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-25 21:38:37 +0800

    Define ChecklistItemSerialiser inheriting from serializers.ModelSerializer

--- tasks/serialisers.py
+++ tasks/serialisers.py
@@ -6,3 +6,8 @@ class TaskSerialiser(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
+
+class ChecklistItemSerialiser(serializers.ModelSerializer):
+    class Meta:
+        model = ChecklistItem
+        fields = ['item']
