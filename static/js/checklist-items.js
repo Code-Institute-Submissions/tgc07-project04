d993301424640b880a3d196b28a2a40e04681636
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-26 20:58:29 +0800

    Display checklist items progress using javascript

--- static/js/checklist-items.js
+++ static/js/checklist-items.js
@@ -1,5 +1,7 @@
-let teamId = document.querySelector('#task-id').name;
-let taskId = document.querySelector('#task-id').value;
+const teamId = document.querySelector('#task-id').name;
+const taskId = document.querySelector('#task-id').value;
+const progressBar = document.querySelector('.progress-bar');
+const progressBarSpan = document.querySelector('.progress-bar-text');
 
 // https://docs.djangoproject.com/en/3.1/ref/csrf/#ajax
 function getCookie(name) {
@@ -19,6 +21,24 @@ function getCookie(name) {
 }
 const csrftoken = getCookie('csrftoken');
 
+// Function to make AJAX call to get checklist items count and checked items count
+function displayChecklistItemsProgress(teamId, taskId){
+    fetch(`/tasks/api/${teamId}/${taskId}/count-checklist-items/`,
+        {
+            method: 'GET',
+            mode: 'same-origin',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            }
+        }
+    ).then(response => response.json()
+    ).then(data => {
+        progressBar.style.width = `${Math.floor(data.checked_items_count/data.items_count*100)}%`;
+        progressBar.setAttribute('aria-valuenow', Math.floor(data.checked_items_count/data.items_count*100));
+        progressBarSpan.innerText = `${data.checked_items_count}/${data.items_count}`;
+    })
+}
+
 // Function to make AJAX call to create checklist item
 function createChecklistItem(teamId, taskId){
     let checklistItemInput = document.querySelector('#add-checklist-item-input');
@@ -36,6 +56,9 @@ function createChecklistItem(teamId, taskId){
     ).then( () => {
         // Reset input field value at end
         checklistItemInput.value = "";
+    }).then( () => {
+        // Get checked items count and display progress
+        displayChecklistItemsProgress(teamId, taskId);
     })
 }
 
@@ -51,7 +74,11 @@ function updateCheckbox(teamId, checklistId, checkboxChecked){
                 'X-CSRFToken': csrftoken
             }
         }
-    ).then(response => response.json())
+    ).then(response => response.json()
+    ).then( () => {
+        // Get checked items count and display progress
+        displayChecklistItemsProgress(teamId, taskId);
+    })
 }
 
 // Function to make AJAX call to update checklist item
@@ -80,7 +107,11 @@ function deleteChecklistItem(teamId, checklistId){
                 'X-CSRFToken': csrftoken
             }
         }
-    ).then(response => response.json())
+    ).then(response => response.json()
+    ).then( () => {
+        // Get checked items count and display progress
+        displayChecklistItemsProgress(teamId, taskId);
+    })
 }
 
 // Function to make AJAX call to get all checklist items belonging to taskId
@@ -172,5 +203,6 @@ window.addEventListener('load', () => {
     
     // Get all checklist items belogning to task
     readChecklistItems(teamId, taskId);
-    
+    // Get checked items count and display progress
+    displayChecklistItemsProgress(teamId, taskId);
 });
