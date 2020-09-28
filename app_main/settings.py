8253fad9d117f438635efd7e8b7a9576c61e7081
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-09-28 09:09:13 +0800

    Only load whitenoise staticfiles directory if in production mode

--- app_main/settings.py
+++ app_main/settings.py
@@ -147,10 +147,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
+# whitenoise static files directory for heroku deployment
+STATIC_ROOT = "" if os.environ.get('DEBUG_MODE')=="True" else os.path.join(BASE_DIR, 'staticfiles')
 
 
 AUTHENTICATION_BACKENDS = (
