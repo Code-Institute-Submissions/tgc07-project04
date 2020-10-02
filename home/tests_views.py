cc42d07cf6537795384d106085951f01ae51c5e1
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-10-02 23:46:33 +0800

    Define test to check pricing view and update user_profile test to log user in

--- home/tests_views.py
+++ home/tests_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 class HomeViewTestCase(TestCase):
     def test_get_response(self):
@@ -7,7 +8,24 @@ class HomeViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home/index.html')
 
+class PricingViewTestCase(TestCase):
+    def test_get_response(self):
+        response = self.client.get(reverse('pricing_route'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/pricing.html')
+
 class UserProfileViewTestCase(TestCase):
+    def setUp(self):
+        # Create user instance
+        self.team_member = User(
+            username = "test_team_member",
+            email = "team_member@mailinator.com",
+            password = "pass123word"
+        )
+        self.team_member.save()
+        # Log in user
+        self.client.force_login(self.team_member, backend=None)
+
     def test_get_response(self):
         response = self.client.get(reverse('user_profile_route'))
         self.assertEqual(response.status_code, 200)
